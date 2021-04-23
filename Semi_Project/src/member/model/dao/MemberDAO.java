8f401c4951318776d7ed2bdc25fd78ef57ea1c38
OhJinYoung <shp04139@naver.com>
2021-04-23 11:58:10 +0900

    2021-04-23 11:58 로그인을 위한 부분

--- Semi_Project/src/member/model/dao/MemberDAO.java
+++ Semi_Project/src/member/model/dao/MemberDAO.java
@@ -1,6 +1,6 @@
 package member.model.dao;
 
-import static common.JDBCTemplate.close;
+import static common.JDBCTemplate.*;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.Properties;
 
 import member.model.vo.Member;
-import member.model.vo.Order;
+import test.model.vo.Test;
 
 public class MemberDAO {
 	private Properties prop = new Properties();
@@ -30,6 +30,44 @@ public class MemberDAO {
 			e.printStackTrace();
 		}
 
+	}
+	
+	public Member loginMember(Connection conn, Member member) {
+		  PreparedStatement pstmt = null;
+		  ResultSet rset = null; 
+		  Member loginUser=null;
+		  
+		  String query = prop.getProperty("loginMember");
+		  
+		  try { 
+			  pstmt = conn.prepareStatement(query); 
+			  pstmt.setString(1,member.getUserId()); 
+			  pstmt.setString(2, member.getPassword()); 
+			  rset = pstmt.executeQuery();
+		  
+		  if (rset.next()) {
+			  			System.out.println(rset);
+						loginUser = new Member(rset.getString("USER_NO"),
+											rset.getString("PASSWORD"),
+											rset.getString("GENDER").charAt(0), 
+											rset.getString("USER_ID"), 
+											rset.getString("USER_NAME"),
+											rset.getDate("BIRTH"), 
+											rset.getString("PHONE"), 
+											rset.getString("EMAIL"),
+											rset.getDate("USER_DATE"), 
+											rset.getString("AUTHORITY").charAt(0),
+											rset.getString("STATUS")); 
+						}
+		  }catch (SQLException e){ 
+			  e.printStackTrace();
+		  }finally { 
+			  close(rset); 
+			  close(pstmt); 
+		  }
+		  
+		  return loginUser;
+		 
 	}
 
 	public Member selectMember(Connection conn, String userId) {
@@ -81,57 +119,6 @@ public class MemberDAO {
 			e.printStackTrace();
 		} finally {
 			close(stmt);
-		}
-		return list;
-	}
-
-	public ArrayList<Order> orderList(Connection conn) {
-		Statement stmt = null;
-		ResultSet rset = null;
-		ArrayList<Order> list = new ArrayList<>();
-
-		String query = prop.getProperty("orderList");
-
-		try {
-			stmt = conn.createStatement();
-			rset = stmt.executeQuery(query);
-
-			while (rset.next()) {
-				Order order = new Order(rset.getInt("order_no"), rset.getString("state"), rset.getString("orderdate"),
-						rset.getString("user_name"), rset.getString("user_id"), rset.getString("productlist"),
-						rset.getInt("price"));
-				list.add(order);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			close(stmt);
-		}
-		return list;
-	}
-
-	public ArrayList<Order> searchOrder(Connection conn, String filter, String input) {
-		PreparedStatement pstmt = null;
-		ResultSet rset = null;
-		ArrayList<Order> list = new ArrayList<>();
-
-		String query = prop.getProperty("searchOrder"+filter);
-
-		try {
-			pstmt = conn.prepareStatement(query);
-			pstmt.setString(1, "%"+input+"%");
-			rset = pstmt.executeQuery();
-
-			while (rset.next()) {
-				Order order = new Order(rset.getInt("order_no"), rset.getString("state"), rset.getString("orderdate"),
-						rset.getString("user_name"), rset.getString("user_id"), rset.getString("productlist"),
-						rset.getInt("price"));
-				list.add(order);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			close(pstmt);
 		}
 		return list;
 	}
--- Semi_Project/src/member/model/service/MemberService.java
+++ Semi_Project/src/member/model/service/MemberService.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 
 import member.model.dao.MemberDAO;
 import member.model.vo.Member;
-import member.model.vo.Order;
 
 public class MemberService {
 
@@ -21,31 +20,20 @@ public class MemberService {
 
 		return member;
 	}
+	
+	public Member loginMember(Member member) { 
+		Connection conn = getConnection();
+		Member loginUser = new MemberDAO().loginMember(conn, member);
+
+		close(conn);
+
+		return loginUser; 
+	}
 
 	public ArrayList<Member> memberList() {
 		Connection conn = getConnection();
 
 		ArrayList<Member> list = new MemberDAO().memberList(conn);
-
-		close(conn);
-
-		return list;
-	}
-
-	public ArrayList<Order> orderList() {
-		Connection conn = getConnection();
-
-		ArrayList<Order> list = new MemberDAO().orderList(conn);
-
-		close(conn);
-
-		return list;
-	}
-
-	public ArrayList<Order> searchOrder(String filter, String input) {
-		Connection conn = getConnection();
-
-		ArrayList<Order> list = new MemberDAO().searchOrder(conn,filter,input);
 
 		close(conn);
 
