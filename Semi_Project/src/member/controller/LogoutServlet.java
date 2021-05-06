92a7baee6092d67d3b6c2823197592b21ccba825
OhJinYoung <shp04139@naver.com>
2021-05-06 09:26:31 +0900

    2021-05-06 09:26 헤더 디자인 조금 변경, 관리자 로그아웃 추가

--- Semi_Project/src/member/controller/LogoutServlet.java
+++ Semi_Project/src/member/controller/LogoutServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class LogoutServlet
  */
-@WebServlet("/logout.main")
+@WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
