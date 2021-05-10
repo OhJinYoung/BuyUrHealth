b05074acd84368ebe023fd6a958373303e43da3b
saeriiin <47206480+saeriiin@users.noreply.github.com>
2021-05-11 07:28:40 +0900

    제품보기 수정

---
+++ Semi_Project/WebContent/WEB-INF/views/product/controller/ProductDetailServlet.java
@@ -0,0 +1,64 @@
+package product.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import product.model.service.ProductService;
+import product.model.vo.Product;
+import product.model.vo.ProductFile;
+
+/**
+ * Servlet implementation class ProductDetailServlet
+ */
+@WebServlet("/detail.pro")
+public class ProductDetailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductDetailServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int pNo = Integer.parseInt(request.getParameter("pNo"));
+		
+		
+		ProductService service = new ProductService();
+		
+		Product product = service.selectProduct(pNo);
+		ArrayList<ProductFile> fileList = service.selectProductFiles(pNo);
+		
+	
+		String page = null;
+		if(fileList != null) {
+			request.setAttribute("product", product);
+			request.setAttribute("fileList", fileList);
+			page = "WEB-INF/views/product/prodDetail.jsp";
+		} else {
+			request.setAttribute("msg",  "제품 상세조회에 실패했습니다");
+			page = "WEB-INF/views/common/errorPage.jsp";
+		}
+		request.getRequestDispatcher(page).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
